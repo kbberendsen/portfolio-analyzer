365ac3e8c27a8649bf9ddbd029019f30a24f4525
Kas Berendsen <kbberendsen@gmail.com>
2024-12-16 21:12:47 +0100

    fix YTD calc (relative to end last year)

--- dashboard.py
+++ dashboard.py
@@ -269,7 +269,7 @@ elif tab_selection == "Daily":
         )
 
     # Filter data by date range
-    daily_filtered_df = daily_product_df[(daily_product_df['End Date'] >= selected_start_date) & (daily_product_df['End Date'] <= selected_end_date)]
+    daily_filtered_df = daily_product_df[(daily_product_df['End Date'] >= selected_start_date) & (daily_product_df['End Date'] <= selected_end_date)].sort_values(by='End Date')
 
     # Top level metrics
     if not daily_filtered_df.empty:
@@ -337,32 +337,22 @@ elif tab_selection == "Daily":
 
         # Top net return YTD
 
-        # Filter rows for 2024
-        ytd_rows = product_df[product_df['End Date'].dt.year == datetime.now().year]
+        # Get last year rows (for returns/cost at the very end of last year)
+        last_year_rows = daily_product_df[daily_product_df['End Date'].dt.year == (datetime.now().year-1)].sort_values(by='End Date')
 
-        # Get the value for January 1st or the first available row in 2024
         top_net_return_ytd_start = (
-            ytd_rows[ytd_rows['End Date'] == f'{datetime.now().year}-01-01 00:00:00']['Net Return (€)']
-            .iloc[0] if not ytd_rows.empty and f'{datetime.now().year}-01-01 00:00:00' in ytd_rows['End Date'].values
-            else ytd_rows['Net Return (€)'].iloc[0] if not ytd_rows.empty
+            last_year_rows.iloc[-1].get('Net Return (€)', 0) if not last_year_rows.empty
             else 0
         )
 
         top_total_cost_ytd_start = (
-            ytd_rows[ytd_rows['End Date'] == f'{datetime.now().year}-01-01 00:00:00']['Total Cost (€)']
-            .iloc[0] if not ytd_rows.empty and f'{datetime.now().year}-01-01 00:00:00' in ytd_rows['End Date'].values
-            else ytd_rows['Total Cost (€)'].iloc[0] if not ytd_rows.empty
+            last_year_rows.iloc[-1].get('Total Cost (€)', 0) if not last_year_rows.empty
             else 0
         )
-
-        top_total_cost_end = daily_filtered_df.iloc[-1].get('Total Cost (€)', 0)
-
-        #top_net_return_ytd_start = product_df[product_df['End Date']==f'{datetime.now().year}-01-01 00:00:00'].iloc[0].get('Net Return (€)', 0)
-        #top_total_cost_ytd_start = product_df[product_df['End Date']==f'{datetime.now().year}-01-01 00:00:00'].iloc[0].get('Total Cost (€)', 0)
-
+        
         if top_total_cost_ytd_start != 0:
             top_net_return_ytd_delta_eur = round((top_net_return_end-top_net_return_ytd_start), 2)
-            top_net_return_ytd_delta_per = round(((top_net_return_end-top_net_return_ytd_start)/abs(top_total_cost_end))*100, 2)
+            top_net_return_ytd_delta_per = round(((top_net_return_end-top_net_return_ytd_start)/abs(top_total_cost_ytd_start))*100, 2)
         else:
             top_net_return_ytd_delta_eur = 0
             top_net_return_ytd_delta_per = 0
