5165e9b8e0901f785f2371e799ae87b7fde19775
Kas Berendsen <kbberendsen@gmail.com>
2025-03-08 19:18:01 +0100

    run db refresh when local cache is empty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import time
+import os
+import subprocess
 from helpers.transactions import transactions
 from helpers.portfolio_analyzer import PortfolioAnalyzer
 from helpers.portfolio import calc_portfolio
@@ -6,10 +8,19 @@ from helpers.portfolio import calc_portfolio
 # Start timing
 start_time = time.time()
 
-# Intialize PortfolioAnalyzer and DB
+# Check if 'output' folder is empty, else initial load from db
+output_folder = "output"
+if not os.listdir(output_folder):
+    print("Output folder is empty. Running db_refresh.py...")
+    subprocess.run(["python", "db_refresh.py"])
+else:
+    print("Output folder is not empty. Skipping db_refresh.py...")
+
+# Initialize PortfolioAnalyzer and DB
 analyzer = PortfolioAnalyzer(transactions)
 
-calc_portfolio(analyzer, '2020-11-26')
+calc_portfolio(analyzer, "2020-11-26")
+
 # End timing
 end_time = time.time()
 print(f"Execution time: {round(end_time - start_time, 2)} seconds")
