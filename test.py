afcbca9c18e304eb27a3f6d360d9e13a35d0cfb3
Kas <kbberendsen@gmail.com>
2025-03-10 17:53:06 +0100

    remove test

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-
-# Load monthly and daily data
-df = pd.read_parquet(os.path.join('output', 'portfolio_monthly.parquet'))
-daily_df = pd.read_parquet(os.path.join('output', 'portfolio_performance_daily.parquet'))
-
-print(df.head())
