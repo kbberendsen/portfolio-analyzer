0ff7a32422fc4d60ad4ebd7f7dd1d1eee984e7f7
Kas <kbberendsen@gmail.com>
2025-05-29 10:26:24 +0200

    analysis page update (badges+columns)

--- app_pages/analysis.py
+++ app_pages/analysis.py
@@ -49,10 +49,6 @@ if os.path.exists(portfolio_file):
         selected_date = st.sidebar.date_input("Select End Date", default_selected_date, min_value=df["End Date"].min(), max_value=df["End Date"].max())
         selected_date = pd.to_datetime(selected_date)
 
-        # Metric selection
-        metric_options = ['Current Value (€)', 'Net Return (€)', 'Total Cost (€)']
-        selected_metric = st.sidebar.selectbox("Select a Performance Metric", metric_options)
-
         # Add holdings type selector
         holdings_option = st.radio(
             "Select Holdings View",
@@ -61,82 +57,163 @@ if os.path.exists(portfolio_file):
             help="Current Holdings only include products with a non-zero current value"
         )
 
-        # Keep going back one day if no data is found
-        while selected_date >= df["End Date"].min():
-            filtered_df = df[df['End Date'] == selected_date]
+        filtered_df = df[df['End Date'] <= selected_date]
+        if filtered_df.empty:
+            st.error("No data found for the selected date. Please select a different date.")
+            st.stop()
+        
+        # Get recent two distinct dates
+        filtered_dates = filtered_df['End Date'].unique()[-2:]
+        filtered_dates = sorted(filtered_dates)
+        date_0 = filtered_dates[0]
+        date_1 = filtered_dates[1] if len(filtered_dates) > 1 else date_0
 
-            if not filtered_df.empty:
-                break  # Stop once at the first day with data
+        # Top net return
+        top_net_return_start = filtered_df[filtered_df['End Date'] == date_0].get('Net Return (€)', 0).sum()
+        top_net_return_end = filtered_df[filtered_df['End Date'] == date_1].get('Net Return (€)', 0).sum()
 
-            selected_date -= pd.Timedelta(days=1)  # Go back one day
+        if top_net_return_start != 0:
+            top_net_return_delta = round((top_net_return_end-top_net_return_start), 2)
+            top_net_return_delta_eur = f"+€ {abs(top_net_return_delta)}" if top_net_return_delta > 0 else f"-€ {abs(top_net_return_delta)}"
+            top_net_return_delta_per = round(((top_net_return_end-top_net_return_start)/abs(top_net_return_start))*100, 2)
+        else:
+            top_net_return_delta_eur = 0
+            top_net_return_delta_per = 0
+
+        # Top current value
+        top_current_value_start = filtered_df[filtered_df['End Date'] == date_0].get('Current Value (€)', 0).sum()
+        top_current_value_end = filtered_df[filtered_df['End Date'] == date_1].get('Current Value (€)', 0).sum()
+
+        if top_current_value_start != 0:
+            top_current_value_delta = round((top_current_value_end-top_current_value_start), 2)
+            top_current_value_delta_eur = f"+€ {abs(top_current_value_delta)}" if top_current_value_delta > 0 else f"-€ {abs(top_current_value_delta)}"
+            top_current_value_delta_per = round(((top_current_value_end-top_current_value_start)/(top_current_value_start))*100, 2)
+        else:
+            top_current_value_delta_eur = 0
+            top_current_value_delta_per = 0
+
+        # Today profit/loss
+        today_pl = top_net_return_delta
+        today_pl_per = top_net_return_delta_per
+
+        # Filter the DataFrame for the recent selected date
+        filtered_df = filtered_df[filtered_df['End Date'] == date_1]
+
+        # Total profit/loss
+        total_pl = filtered_df['Net Return (€)'].sum()
+        total_pl_per = filtered_df['Net Return (€)'].sum() / filtered_df['Total Cost (€)'].sum() * 100 if filtered_df['Total Cost (€)'].sum() != 0 else 0
         
         # Filter based on holdings option
         if holdings_option == "Current Holdings":
             filtered_df = filtered_df[filtered_df["Quantity"] != 0]
 
-        # Prepare stacked bar chart data
-        stacked_df = filtered_df.copy()
+        # Prepare display df
+        display_df = filtered_df.copy()
 
-        # Only select relevant columns for the stacked bar chart
-        stacked_df = stacked_df[['Product', 'Quantity', 'Current Value (€)', 
+        # Only select relevant columns
+        display_df = display_df[['Product', 'Quantity', 'Current Value (€)', 
                                  'Net Return (€)', 'Net Performance (%)', 'Total Cost (€)'
                         ]]
 
-         # Percentage of total
-        stacked_df["Current Allocation %"] = stacked_df['Current Value (€)'] / stacked_df['Current Value (€)'].sum() * 100
+        # Create new column with 30-day Net Performance (%) trend as list
+        date_L30 = date_1 - timedelta(days=30)
+        display_df["Net Performance (%) - Trend"] = display_df.apply(
+            lambda row: df[
+                (df["Product"] == row["Product"]) &
+                (df["End Date"] >= date_L30) &
+                (df["End Date"] <= date_1)
+            ]["Net Performance (%)"].tolist(),
+            axis=1
+        )
+
+        # Allocation
+        display_df["Current Allocation %"] = display_df['Current Value (€)'] / display_df['Current Value (€)'].sum() * 100
 
         # Sort products by allocation and then by total cost
-        stacked_df = stacked_df.sort_values(
+        display_df = display_df.sort_values(
             by=['Current Allocation %', 'Total Cost (€)'],
             ascending=[False, False]
         )
 
-        df_height_px = 50*len(stacked_df)+35
+        df_height_px = 50*len(display_df)+37
 
         # Custom styling function
         def color_net_performance(val):
             color = '#09ab3b' if val > 0 else '#ff2b2b' if val < 0 else 'gray'
             return f'color: {color}'
+        
+        # Final column order
+        display_df = display_df[['Product', 'Current Allocation %' ,'Quantity', 'Current Value (€)', 
+                                 'Net Return (€)', 'Net Performance (%)', 'Net Performance (%) - Trend', 'Total Cost (€)'
+                        ]]
 
-        # Apply Styler to the "Net Performance (%()" column
-        stacked_df = stacked_df.style.map(color_net_performance, subset=["Net Performance (%)"])
+        # Apply Styler to the "Net Performance" columns
+        display_df = display_df.style.map(color_net_performance, subset=["Net Performance (%)", "Net Return (€)"])
+
+    # Top badges
+    badge_value_color = 'green' if top_current_value_delta > 0 else 'red' if top_current_value_delta < 0 else 'gray'
+    badge_value_icon = ':material/arrow_upward:' if top_current_value_delta > 0 else ':material/arrow_downward:' if top_current_value_delta < 0 else ':material/info:'
+    badge_value_text = f"Total Portfolio: € {abs(top_current_value_end):.2f} (∆ +{top_current_value_delta_per}% | {top_current_value_delta_eur}) "if top_current_value_end > 0 \
+                    else f"Total Portfolio: € {abs(top_current_value_end):.2f} (∆ -{top_current_value_delta_per}% | {top_current_value_delta_eur}) " if top_current_value_end < 0 \
+                    else "Portfolio Empty"
+
+    badge_total_pl_color = 'green' if total_pl > 0 else 'red' if total_pl < 0 else 'gray'
+    badge_total_pl_icon = ':material/arrow_upward:' if total_pl > 0 else ':material/arrow_downward:' if total_pl < 0 else ':material/info:'
+    badge_total_pl_text = f"All-time Profit: {total_pl_per:.2f}% (€ {abs(total_pl):.2f})" if total_pl > 0 \
+                    else f"All-time Loss: {total_pl_per:.2f}% (-€ {abs(total_pl):.2f})" if total_pl < 0 \
+                    else "No Profit/Loss"
+    
+    st.markdown(f":{badge_value_color}-badge[{badge_value_icon} {badge_value_text}] :{badge_total_pl_color}-badge[{badge_total_pl_icon} {badge_total_pl_text}]")
 
     # Show dataframe
     st.dataframe(
-        stacked_df,
+        display_df,
         height=df_height_px,
         hide_index=True,
         row_height=50,
         column_config={
+                "Product": st.column_config.TextColumn(
+                    "Product",
+                    width="medium",
+                    pinned=True,
+                ),
+
                 "Current Allocation %": st.column_config.ProgressColumn(
-                    "Current Allocation %",
+                    "Allocation (%)",
                     format="%.1f%%",
                     min_value=0,
                     max_value=100,
-                    width="small"
+                    width="small",
+                    help="Current allocation percentage of the product in the portfolio (product current value / total current value)."
                 ),
                 "Current Value (€)": st.column_config.NumberColumn(
                     "Current Value (€)",
-                    format="€ %.2f"
-                ),
-                "Net Return (€)": st.column_config.ProgressColumn(
-                    "Net Return (€)",
                     format="€ %.2f",
-                    min_value=0,
-                    max_value=filtered_df['Net Return (€)'].sum(),
-                    width="small"
+                    width="small",
+                ),
+                "Net Return (€)": st.column_config.NumberColumn(
+                    "Profit/Loss (€)",
+                    format="€ %.2f",
+                    width="small",
                 ),
                 "Total Cost (€)": st.column_config.NumberColumn(
                     "Total Cost (€)",
-                    format="€ %.2f"
+                    format="€ %.2f",
+                    width="small"
                 ),
                 "Quantity": st.column_config.NumberColumn(
                     "Quantity",
-                    format="%d"
+                    format="%d",
+                    width="small"
                 ),
                 "Net Performance (%)": st.column_config.NumberColumn(
-                    "Net Performance (%)",
-                    format="%.2f%%"
+                    "Profit/Loss (%)",
+                    format="%.2f%%",
+                    width="small"
+                ),
+                "Net Performance (%) - Trend": st.column_config.AreaChartColumn(
+                    "30-day P/L (%)",
+                    width="small"
                 )
         }
     )
